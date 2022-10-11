ecd64031bc5990621819a579719254f6dd148030
sadia <sadiaislam1363@gmail.com>
2022-10-11 19:04:37 +0600

    update the another one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
    <h1 className='purple'>Purple Sunflower</h1>
      </div>
      <div>
-     <Link to='/home'>Home</Link>
+     <Link to='/'>Home</Link>
      <Link to='/statistics'>Statistics</Link>
      <Link to='/blog'>Blog</Link>
      </div>
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Option from '../Option/Option';
+import { EyeIcon } from '@heroicons/react/24/solid'
 import './Question.css'
 
 const Question = ({ques}) => {
     const{correctAnswer,options,question}=ques;
     
+    
     return (
-        <div className='ques-container'>
-           <h3> {question}</h3>
+        <div  className='ques-container'>
+   
+      <h3> {question} </h3>
+      <p><EyeIcon onClick={()=> alert(correctAnswer)} className='eye-icon'/>
+      </p>
+    
            {
        options.map(option=> <Option
        key={option.id}
